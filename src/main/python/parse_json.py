eca3c774df31bbd0b63fd923240f4cd52f6ab5ba
Paul Breuil <tykapl.breuil@gmail.com>
2024-02-24 13:26:54 +0100

    more progress

--- src/main/python/parse_json.py
+++ src/main/python/parse_json.py
@@ -2,10 +2,41 @@ import json
 from tqdm import tqdm
 import networkx as nx
 
+TAXONS_TO_KEEP = {
+    "Q38348": "phylum",
+    "Q164280": "subfamily",
+    "Q227936": "tribe",
+    "Q5867051": "subclass",
+    "Q2889003": "infraorder",
+    "Q35409": "family",
+    "Q3238261": "subgenus",
+    "Q5868144": "superorder",
+    "Q37517": "class",
+    "Q3504061": "superclass",
+    "Q2752679": "subkingdom",
+    "Q2136103": "superfamily",
+    "Q2111790": "superphylum",
+    "Q7432": "species",
+    "Q68947": "subspecies",
+    "Q34740": "genus",
+    "Q1153785": "subphylum",
+    "Q3965313": "subtribe",
+    "Q36602": "order",
+    "Q2007442": "infraclass",
+    "Q3150876": "infrakingdon",
+    "Q6311258": "pavorder",
+    "Q36732": "kindgom",
+    "Q5867959": "suborder"
+}
+
+DOUBLE_TAXONS = []
+
 def parse_large_json_file(file_path):
+    data = {}
     G = nx.DiGraph()
     # Get the total number of lines in the file for progress tracking
     total_lines = sum(1 for line in open(file_path))
+    mul_rank = 0
 
     with open(file_path, 'r') as f:
         # Initialize tqdm progress bar
@@ -15,7 +46,6 @@ def parse_large_json_file(file_path):
         # Iterate over each line in the file
         for line in f:
             if (len(line) < 3):
-                print(line)
                 progress_bar.update(1)
                 continue
 
@@ -38,13 +68,49 @@ def parse_large_json_file(file_path):
                 G.add_edge(parent_id, json_data["id"])
                 G.nodes[json_data["id"]]["site_link"] = json_data["sitelinks"].get("enwiki") != None
 
+            filtered_data = {}
+            filtered_data["site_link"] = json_data["sitelinks"].get("enwiki") != None
+
+            rank_claims = json_data["claims"].get("P105")
+            ranks = []
+            if rank_claims:
+                for rank_prop in rank_claims:
+                    rank_snak = rank_prop["mainsnak"]
+                    if (rank_snak.get("datavalue") == None):
+                        continue
+                    rank_id = rank_snak["datavalue"]["value"]["id"]
+                    rank = TAXONS_TO_KEEP.get(rank_id)
+                    if rank:
+                        ranks.append(rank)
+                if len(ranks) > 1:
+                    mul_rank += 1
+                    DOUBLE_TAXONS.append(json_data["id"])
+                if ranks:
+                    filtered_data["rank"] = ranks[0]
+            data[json_data["id"]] = filtered_data
+
             # Update progress bar
             progress_bar.update(1)
 
         # Close progress bar after completion
         progress_bar.close()
+        print(mul_rank)
         print("Making animalia tree...")
-        return nx.bfs_tree(G, "Q729", reverse=False, depth_limit=None, sort_neighbors=None)
+        animalia_tree = nx.bfs_tree(G, "Q729", reverse=False, depth_limit=None, sort_neighbors=None)
+        for node in animalia_tree.nodes():
+            for key in data[node]:
+                animalia_tree.nodes[node][key] = data[node][key]
+        print(animalia_tree.nodes["Q729"])
+        DOUBLE_TAXONS_ANIMALIA = []
+        DOUBLE_TAXONS_ANIMALIA_WIKI = []
+        for taxon in DOUBLE_TAXONS:
+            if animalia_tree.has_node(taxon):
+                DOUBLE_TAXONS_ANIMALIA.append(taxon)
+                if animalia_tree.nodes[taxon]["site_link"]:
+                    DOUBLE_TAXONS_ANIMALIA_WIKI.append(taxon)
+        print("DOUBLE_TAXONS_ANIMALIA", len(DOUBLE_TAXONS_ANIMALIA), DOUBLE_TAXONS_ANIMALIA)
+        print("DOUBLE_TAXONS_ANIMALIA_WIKI", len(DOUBLE_TAXONS_ANIMALIA_WIKI), DOUBLE_TAXONS_ANIMALIA_WIKI)
+        return animalia_tree
 
 if __name__ == "__main__":
     file_path = 'results/json-all-taxon.json'
--- src/main/python/transform_graph.py
+++ src/main/python/transform_graph.py
@@ -1,8 +1,71 @@
 import networkx as nx
 from time import time
 
+TAXONS_TO_KEEP = {
+    "Q38348": "phylum",
+    "Q164280": "subfamily",
+    "Q227936": "tribe",
+    "Q5867051": "subclass",
+    "Q2889003": "infraorder",
+    "Q35409": "family",
+    "Q3238261": "subgenus",
+    "Q5868144": "superorder",
+    "Q37517": "class",
+    "Q3504061": "superclass",
+    "Q2752679": "subkingdom",
+    "Q2136103": "superfamily",
+    "Q2111790": "superphylum",
+    "Q7432": "species",
+    "Q68947": "subspecies",
+    "Q34740": "genus",
+    "Q1153785": "subphylum",
+    "Q3965313": "subtribe",
+    "Q36602": "order",
+    "Q2007442": "infraclass",
+    "Q3150876": "infrakingdon",
+    "Q6311258": "pavorder",
+    "Q36732": "kindgom",
+    "Q5867959": "suborder"
+}
+
+
 if __name__ == "__main__":
     t = time()
     animalia_tree = nx.read_graphml("results/animalia_tree.graphml")
     print(time() - t)
-    print(animalia_tree)
+    print("Tree of animalia : ", animalia_tree)
+    total_site_links = 0
+    site_links_per_rank = {}
+    count_per_rank = {}
+    prop_ranks = {}
+    no_rank = 0
+    for rank_id in TAXONS_TO_KEEP:
+        rank = TAXONS_TO_KEEP[rank_id]
+        site_links_per_rank[rank] = 0
+        count_per_rank[rank] = 0
+        prop_ranks[rank] = 0
+    for node in animalia_tree.nodes():
+        node_data = animalia_tree.nodes[node]
+        if node_data["site_link"]:
+            total_site_links += 1
+        if node_data.get("rank"):
+            count_per_rank[node_data["rank"]] += 1
+            if node_data["site_link"]:
+                site_links_per_rank[node_data["rank"]] += 1
+        else:
+            no_rank += 1
+
+    def hasSiteLink(node):
+        return animalia_tree.nodes[node].get("site_link") == True or not animalia_tree.nodes[node].get("rank") or animalia_tree.nodes[node].get("rank") == "subgenus"
+
+    only_wiki = nx.subgraph_view(animalia_tree, filter_node=hasSiteLink)
+    only_wiki_tree = nx.bfs_tree(only_wiki, "Q729", reverse=False, depth_limit=None, sort_neighbors=None)
+    print("Animalia tree of connected wiki pages: ", only_wiki_tree)
+    print("Number of wiki pages :", total_site_links)
+    print("Wikipage per taxon :", site_links_per_rank)
+    print("Total count per taxon :", count_per_rank)
+    for rank_id in TAXONS_TO_KEEP:
+        rank = TAXONS_TO_KEEP[rank_id]
+        prop_ranks[rank] = site_links_per_rank[rank] / count_per_rank[rank]
+    print("Wikipage proportion per taxon :", prop_ranks)
+    print("Taxons with no rank :", no_rank)
