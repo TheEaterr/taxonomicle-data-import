3307e8d4bf64a981a2a244512f4e09aaab2444d9
Paul Breuil <tykapl.breuil@gmail.com>
2024-04-19 20:03:51 +0200

    add admin authentication to data upload

--- src/main/js/utils.js
+++ src/main/js/utils.js
@@ -1,34 +1,46 @@
-import fs from 'fs';
-import PocketBase from 'pocketbase';
+import fs from "fs";
+import PocketBase from "pocketbase";
+import {
+    ADMIN_EMAIL, ADMIN_PASSWORD
+} from "secret"
 
-export const pb = new PocketBase('http://127.0.0.1:8090');
+export const pb = new PocketBase("http://127.0.0.1:8090");
 
+const authData = await pb.admins
+  .authWithPassword(ADMIN_EMAIL, ADMIN_PASSWORD)
+
+if (!pb.authStore.isValid) {
+    console.error("Authentication to PocketBase failed");
+    process.exit(1);
+}
 
 // Function to read JSON file synchronously
 export function readJSONFile(filename) {
-    try {
-        const data = fs.readFileSync(filename, 'utf8');
-        const jsonData = JSON.parse(data);
-        return jsonData;
-    } catch (error) {
-        console.error('Error reading JSON file:', error.message);
-        return undefined;
-    }
+  try {
+    const data = fs.readFileSync(filename, "utf8");
+    const jsonData = JSON.parse(data);
+    return jsonData;
+  } catch (error) {
+    console.error("Error reading JSON file:", error.message);
+    return undefined;
+  }
 }
 
 export function processString(inputString) {
-    if (inputString.length > 15) {
-        throw new Error('Id ' + inputString + ' is too long (more than 15 characters)');
-    } else {
-        while (inputString.length < 15) {
-            inputString += '_';
-        }
-        return inputString;
+  if (inputString.length > 15) {
+    throw new Error(
+      "Id " + inputString + " is too long (more than 15 characters)"
+    );
+  } else {
+    while (inputString.length < 15) {
+      inputString += "_";
     }
+    return inputString;
+  }
 }
 
 // Replace 'example.json' with the path to your JSON file
-const jsonFilename = '../../../results/animalia_tree.json';
+const jsonFilename = "../../../results/animalia_tree.json";
 
 // Read JSON file
 export const jsonData = readJSONFile(jsonFilename);
