1fae0bf6ea5f7c2ffbda521542a9b1b9cee17828
Paul Breuil <tykapl.breuil@gmail.com>
2024-04-26 18:34:21 +0200

    add preprocessor for smart random taxon generator

---
+++ src/main/js/uploadBigTaxons.js
@@ -0,0 +1,44 @@
+import { pb } from './utils.js';
+import fs from 'fs';
+
+// Uploading probability values for a great random taxon generator
+const getTaxonFilter = (parent) =>
+    `rank.name = "species" && image_path=true && path ~ "${parent}"`;
+const records = await pb.collection('taxon').getFullList({
+    filter: "rank.order >= 30 && parent.rank.order < 30",
+    fields:  '*,description:excerpt(1,true)'
+});
+const bigTaxons = [];
+let sum = 0;
+for (const record of records) {
+    const id = record.id;
+    const taxons = await pb.collection('taxon').getList(1, 1, {
+        expand: 'rank',
+        filter: getTaxonFilter(id),
+    })
+    const probability_root = 3 * Math.log(1 + taxons.totalItems);
+    const probability = probability_root * probability_root
+    bigTaxons.push(
+        {
+            "taxon": id,
+            "name": record.scientific,
+            "count": taxons.totalItems,
+            "probability": probability,
+        }
+    )
+    sum += probability;
+}
+let rolling_sum = 0;
+for (const taxon of bigTaxons) {
+    if (taxon.probability != 0) {
+        rolling_sum += taxon.probability / sum * 10000;
+        taxon.probability = rolling_sum;
+    }
+}
+bigTaxons[bigTaxons.length - 1].probability = 10000;
+pb.autoCancellation(false);
+bigTaxons.forEach(async taxon => {
+    await pb.collection('random_big_taxon').create(taxon);
+})
+fs.writeFileSync('../../../results/random_big_taxon.json', JSON.stringify(bigTaxons));
+
